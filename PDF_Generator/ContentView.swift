a14311fad433f28fbb3d908a1db49aa9eec34e8a
Stanley Pan <imoment.developer@gmail.com>
2022-01-23 14:13:26 -0500

    Initial Commit

---
+++ PDF_Generator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDF_Generator
+//
+//  Created by Stanley Pan on 2022/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PDF_Generator/PDF_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_GeneratorApp.swift
+//  PDF_Generator
+//
+//  Created by Stanley Pan on 2022/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PDF_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
